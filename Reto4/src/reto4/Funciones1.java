c089f1ca1bcc49a1099be9c7fcd95b664361837b
PzDarby <izliquihd@gmail.com>
2024-11-19 11:44:24 +0100

    Comentarios

--- Reto4/src/reto4/Funciones1.java
+++ Reto4/src/reto4/Funciones1.java
@@ -1,15 +1,18 @@
 package reto4;
 
 public class Funciones1 {
+
 	private static String vocalesMayus(String s) {
 		String letra=null;
 		// Comprueba cada letra del string y si es una vocal la pone es mayuscula
 		for (int i = s.length(); i> 0; i--) {
 			 letra=s.substring(i,i);
-			if (!letra.equals("a") || !letra.equals("e") || !letra.equals("i")|| letra.equals("o") || letra.equals("u")) {
+			// Si es vocal lo añade al string en mayusculas
+			 if (!letra.equals("a") || !letra.equals("e") || !letra.equals("i")|| letra.equals("o") || letra.equals("u")) {
 			s=s+s.substring(i,i).toLowerCase();
 			}
 		}
+		// Devuelve el string
 		return letra;
 	}
 }
